8a3580c03833b8f23530abf935ecdf233d292a25
Daksh <dakshagravat1@gmail.com>
2023-06-22 02:42:42 +0530

    bug fixes

--- app/src/main/java/com/example/measuredisplayenergyconsumption/MainActivity.java
+++ app/src/main/java/com/example/measuredisplayenergyconsumption/MainActivity.java
@@ -11,14 +11,12 @@ import android.os.PowerManager.WakeLock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean flag = false;
     private PowerManager powerManager;
     private WakeLock wakeLock;
     private TextView batteryPercentageTextView;
@@ -28,11 +26,16 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler;
     private Runnable batteryRunnable;
     private int startBatteryPercentage;
-    private int  initialBatteryPercentage = -1;
+    private int  lastBatteryPercentage;
     private int consumedPercentage;
+
+    private Button stopButton;
     private long startTime;
 
-    private ProgressBar progressBar;
+    private int finalEplasedSeconds;
+    private TextView startBatteryPercentageTextView;
+    private TextView differenceTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +44,24 @@ public class MainActivity extends AppCompatActivity {
 
         powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Display:BatteryMonitoringWakeLock");
-        progressBar = findViewById(R.id.progressBar);
         batteryPercentageTextView = findViewById(R.id.battery_percentage_textview);
         timeTextView = findViewById(R.id.time_textview);
         startButton = findViewById(R.id.start_button);
-
+        stopButton = findViewById(R.id.stop_button);
+        differenceTextView = findViewById(R.id.difference_textview);
+        startBatteryPercentageTextView = findViewById(R.id.start_battery_percentage_textview);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startBatteryMonitoring();
                 startButton.setEnabled(false);
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopBatteryMonitoring();
             }
         });
 
@@ -65,9 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void startBatteryMonitoring() {
         startButton.setEnabled(false);
-        progressBar.setVisibility(View.VISIBLE);
         wakeLock.acquire();
         startBatteryPercentage = getBatteryPercentage();
+
+        startBatteryPercentageTextView.setText("Start Battery Percentage: " + startBatteryPercentage);
         consumedPercentage = 0;
         startTime = System.currentTimeMillis();
         handler.postDelayed(batteryRunnable, 15000); // Initial delay of 15 seconds
@@ -78,16 +90,21 @@ public class MainActivity extends AppCompatActivity {
         Log.d("CHECK", "checkBatteryPercentage ----- " + currentBatteryPercentage);
 
         batteryPercentageTextView.setText(getString(R.string.battery_percentage, currentBatteryPercentage));
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        int elapsedSeconds = (int) (elapsedTime / 1000);
+        timeTextView.setText(getString(R.string.consumed_time, elapsedSeconds));
+        int difference = currentBatteryPercentage - startBatteryPercentage;
+        differenceTextView.setText("Difference in percentage: " + difference);
+        Log.d("CHECK", "Difference ----- " + difference);
 
-        int difference = currentBatteryPercentage - startBatteryPercentage - consumedPercentage;
         if (difference >= 5) {
+//            lastBatteryPercentage = currentBatteryPercentage;
+            finalEplasedSeconds = elapsedSeconds;
             stopBatteryMonitoring();
-            long elapsedTime = System.currentTimeMillis() - startTime;
-            int elapsedSeconds = (int) (elapsedTime / 1000);
-            timeTextView.setText(getString(R.string.consumed_time, elapsedSeconds));
-        } else {
-            consumedPercentage += difference;
         }
+//        else {
+//            consumedPercentage += difference;
+//        }
 
         handler.postDelayed(batteryRunnable, 15000); // Schedule the next execution after 15 seconds
     }
@@ -95,11 +112,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void stopBatteryMonitoring() {
         handler.removeCallbacks(batteryRunnable);
-        progressBar.setVisibility(View.GONE);
         startButton.setEnabled(true);
         if (wakeLock.isHeld()) {
             wakeLock.release();
         }
+
+        batteryPercentageTextView.setText("Starting Percentage: " + startBatteryPercentage + "\nEnding Battery Percentage: " + lastBatteryPercentage);
+        timeTextView.setText( "Time Taken: " + finalEplasedSeconds);
     }
 
     private int getBatteryPercentage() {
